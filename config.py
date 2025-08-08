365f5b8fd8cd42f36cbc873e1958d489d69c44c3
gianluca pes <pesgianluca20@gmail.com>
2025-08-09 01:38:48 +0200

    fix route massive insert

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import os
 
 class Settings(BaseSettings):
     BASE_URL : str = os.environ.get("BASE_URL",default="")
+    BASE_URL_LOCAL : str = os.environ.get("BASE_URL_LOCAL",default="")
     URL_RETRIEVE_YOUTUBE_CHANNEL : str =  os.environ.get("URL_RETRIEVE_YOUTUBE_CHANNEL",default="")
     URL_RETRIEVE_VIDEO  : str = os.environ.get("URL_RETRIEVE_VIDEO",default="")
+    LOCAL : str = os.environ.get("LOCAL",default="")
 
 
 settings=Settings()
--- routes/massive_insert_channels.py
+++ routes/massive_insert_channels.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 import requests
 from config import settings
 from urllib.parse import urljoin
+from fastapi.responses import JSONResponse
 
 
 
@@ -10,8 +11,13 @@ router = APIRouter()
 @router.post("/massive_insert")
 def massive_insert(channelName : list[str]):
     res=[]
-    url=urljoin(settings.BASE_URL,settings.URL_RETRIEVE_YOUTUBE_CHANNEL)
-    for item in channelName:
-        params={"name":item}
-        res.append(requests.get(url,params))
-    return res
+
+    try:
+        url_retrieve_youtube_channel=urljoin(settings.BASE_URL_LOCAL if settings.LOCAL else settings.BASE_URL,settings.URL_RETRIEVE_YOUTUBE_CHANNEL)
+        for item in channelName:
+            params={"name":item }
+            r=requests.get(url_retrieve_youtube_channel,params)
+            res.append(r.content)
+        return {"message": res}
+    except Exception as e:
+        return JSONResponse(e)
